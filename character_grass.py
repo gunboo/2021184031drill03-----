d0c23eec27958c9c7694a4213ece63f815776cf7
gunboo <wlstjr2517@naver.com>
2024-09-24 11:49:46 +0900

    뼈대 완성 프린트로 무한루프 구조 확인

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,19 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def run_ciecle():
+    print('CIRCLE')
+    pass #내용이 전혀 없는 빈 함수다.
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass    
+
+while True:
+    run_ciecle() #함수로 뼈대를 잡는게 중요하다
+    run_rectangle()
 
 close_canvas()
