8e2d55430b28db5c478a8c518ebd39e3dd79d924
gunboo <wlstjr2517@naver.com>
2024-09-24 12:12:14 +0900

    원 운동 구현 완료

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 grass = load_image('grass.png')
@@ -7,9 +7,16 @@ boy = load_image('character.png')
 
 def run_ciecle():
     print('CIRCLE')
-
+    
+    r, cx, cy = 300, 800//2, 600//2
+    
+    for degree in range(0,360,3):
+        theta = math.radians(degree) 
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+    
     clear_canvas_now()
-    boy.draw_now(400.300)
+    boy.draw_now(x.y)
     delay(0.1)
 
 def run_rectangle():
