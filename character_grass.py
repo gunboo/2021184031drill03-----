493017ae5132f5ad0065a0355a7f2a2e1b0481bd
gunboo <wlstjr2517@naver.com>
2024-09-24 11:56:24 +0900

    캐릭터 화면 출력 구현(서클함수)

--- character_grass.py
+++ character_grass.py
@@ -7,14 +7,18 @@ boy = load_image('character.png')
 
 def run_ciecle():
     print('CIRCLE')
-    pass #내용이 전혀 없는 빈 함수다.
+
+    clear_canvas_now()
+    boy.draw_now(400.300)
+    delay(0.1)
 
 def run_rectangle():
     print('RECTANGLE')
     pass    
 
 while True:
-    run_ciecle() #함수로 뼈대를 잡는게 중요하다
+    run_ciecle() 
     run_rectangle()
+    break
 
 close_canvas()
